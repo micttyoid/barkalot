ca74ec1fcfde4aa4bb1c1899801ebd4534f0d54f
Luke Yoo <w.lukeyoo@gmail.com>
2025-05-11 23:44:05 -0700

    Clean up examples

--- examples/demo_baz.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "foo_lib/addition.h"
-#include "foo_lib/multiplication.h"
-
-int main() {
-    std::cout << "2 + 3 = " << foo_lib::add(2, 3) << "\n";
-    std::cout << "2 * 3 = " << foo_lib::multiply(2, 3) << "\n";
-    return 0;
-}
